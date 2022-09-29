889188cbde922fbf1ebe2f8d44913e8b8c31bd05
Taofeek Bello <bellotaofeeka1910@gmail.com>
2022-09-28 17:30:31 -0700

    lists.h

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -6,32 +6,36 @@
 
 #include <stdlib.h>
 
+#include <stdio.h>
+
+#include <string.h>
+
 
 
 /**
+
+ * struct listint_s - singly linked list
+
+ * @n: integer
+
+ * @next: points to the next node
+
  *
- *  * struct listint_s - singly linked list
+
+ * Description: singly linked list node structure
+
  *
- *   * @n: integer
- *
- *    * @next: points to the next node
- *
- *     *
- *
- *      * Description: singly linked list node structure
- *
- *       * for Holberton project
- *
- *        */
+
+ */
 
 typedef struct listint_s
 
 {
-
-		int n;
-
-			struct listint_s *next;
-
+  
+  int n;
+  
+  struct listint_s *next;
+  
 } listint_t;
 
 
